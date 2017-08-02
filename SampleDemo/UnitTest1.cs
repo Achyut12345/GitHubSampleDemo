fadf9d06aa9089ab1d98cfb187c3601feaa64dc2
Achyut Wadghane (Infosys Ltd) <v-acwadg@microsoft.com>
2017-08-02 13:54:32 +0530

    First

---
+++ SampleDemo/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System.Threading;
+using OpenQA.Selenium.Chrome;
+
+namespace SampleDemo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("www.google.com");
+            Thread.Sleep(3000);
+        }
+    }
+}
